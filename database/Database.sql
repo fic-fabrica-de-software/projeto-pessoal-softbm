aa2c55497de264d2a5aee130d92a7e8248da60f4
miguelrcha <miguellrochaxavier@gmail.com>
2025-10-03 15:37:23 -0300

    Adição tabela usuarios no banco de dados

--- database/Database.sql
+++ database/Database.sql
@@ -2,3 +2,12 @@ CREATE DATABASE IF NOT EXISTS `biafashionkids`;
 
 USE biafashionkids;
 
+CREATE TABLE IF NOT EXISTS `usuarios` (
+    idusuarios INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome_usuarios VARCHAR(87) NOT NULL,
+    email_usuarios VARCHAR(87) NOT NULL,
+    data_usuarios DATE NOT NULL,
+    telefone_usuarios CHAR(11) NOT NULL,
+    cpf_usuarios CHAR(11) NOT NULL,
+    preferencia_usuarios ENUM('Moda Feminina', 'Moda Masculina', 'Prefiro não informar') NOT NULL DEFAULT NULL
+);
