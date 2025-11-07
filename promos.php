e38564420e7e2a066074a35aca23117ad6ee8082
BeatrizCercal <beatriz_cachoeira@estudante.sesisenai.org.br>
2025-11-07 15:13:51 -0300

    Adição + Testes de inserção de Botão +

--- promos.php
+++ promos.php
@@ -4,46 +4,73 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="src/templates/style_promos.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,0,0&icon_names=add" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Promos</title>
 </head>
 <body>
      <section class="product-section">
-        <div class="tshirt-sections"> <a href="modelo_produto.html">
-                <div class="tshirt-photos"> <img src="src/images/imgs-produto/hellokity.png">
+        <div class="tshirt-sections"> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
+                <div class="tshirt-photos"> <img src="src/images/imgs-produto/hellokity.png"> 
                     <p>Sandália HELLO KITTY © Licenciado/Original</p> <span><strong>R$ 61,96</strong></span>
+                    
+                </div> 
+            </a>  <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
+                <div class="tshirt-photos"> <img
+                        src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
+                    <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
+                    
                 </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
                 </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
                 </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
                 </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
                 </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
                 </div>
-            </a> <a href="">
-                <div class="tshirt-photos"> <img
-                        src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
-                    <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
-                </div>
-            </a> <a href="">
+            </a> <a href="" onclick="return confirm('')"> 
+            <span class="material-symbols-outlined">
+add
+</span>
                 <div class="tshirt-photos"> <img
                         src="https://images.tcdn.com.br/img/img_prod/947450/camiseta_basic_color_preto_1021_1_4a739bf8de5955b412d3e004aa92dd20.jpg">
                     <p>Camiseta Comfort Adidas em Algodão Preta</p> <span><strong>R$ 79,90</strong></span>
@@ -53,3 +80,14 @@
 </body>
   <?php include 'src/templates/footer.php' ?>
 </html>
+
+<style>
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' EA33F7,
+  'opsz' 40,
+  'color' EA33F7
+}
+</style>
